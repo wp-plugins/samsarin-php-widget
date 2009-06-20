241874f950b08f0df8e0a538bf4b0e766abb20f8
samsarin <samsarin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-20 18:56:32 +0000

    Complete rewrite to support WP 2.8.
    git-svn-id: https://plugins.svn.wordpress.org/samsarin-php-widget/trunk@127865 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: samsarin
 Tags: php, widget
 Requires at least: 2.1
-Tested up to: 2.5.1
+Tested up to: 2.8
 Stable tag: 1.3.2
 
 A plugin that adds that ability to insert any PHP code into a sidebar widget.
@@ -11,11 +11,11 @@ A plugin that adds that ability to insert any PHP code into a sidebar widget.
 
 The text widget that comes with the wordpress does not allow you to include PHP code. This plugin adds a new widget called `Samsarin PHP Widget`, which will allow you to include PHP code, as well as plain text and HTML.
 
+The current version supports Wordpress 2.8+. To get support for Wordpress 2.1 - 2.7, use version 1.3.2 of the widget.
+
 Features included:
 
-* Add up to 25 `Samsarin PHP Widgets` to your sidebar.
 * Add PHP code to both the widget title and the widget body.
-* Reset allows you to easily revert the contents of a widget.
 
 _Warning: This widget provides great flexibility in customizing your sidebar. It achieves this by automatically evaluating any included PHP included in the title or the body of the widget. Care should be taken when including PHP code in the sidebar, as with anywhere else in the Wordpress system._
 
@@ -39,7 +39,9 @@ You may enter PHP code both in the title and the body.
 
 = What versions of Wordpress are supported? =
 
-This plugin has been tested with Wordpress 2.1, 2.2, 2.3, and 2.5.1.
+The current version is supported with Wordpress 2.8+.
+
+Version 1.3.2 of the plugin is supported with Wordpress 2.1 - 2.7 and has been tested with Wordpress 2.1, 2.2, 2.3, and 2.5.1.
 
 = What do I do if Samsarin PHP Widget does not show up in the widget admin console? =
 
@@ -49,9 +51,3 @@ First, make sure that the plugin is enabled.
 2. Go to Plugins
 3. Verify that "Samsarin PHP Widget" is in the list. If it is not, then wordpress doesn't see the plugin. Is it in your plugin directory (wp-content/plugins)?
 4. Verify that the plugin is enabled
-
-If it is enabled and you can see it on the left hand side of the Design | Widgets page, then it is possible that the content of one of the widgets is causing a problem. With Samsarin PHP Widget version 1.3.2 you can reset the widgets.
-
-1. Backup your database.
-2. Go to Settings | Samsarin PHP
-3. If you know the name of the widget that is not working, use the "Reset" option. If you do not know which widget is failing, use the "Reset All" option. __This will reset the widget to its initial, empty state.__
--- samsarin-php-widget.php
+++ samsarin-php-widget.php
@@ -1,12 +1,13 @@
 <?php
 /*
 Plugin Name:    Samsarin PHP Widget
-Plugin URI:     http://blog.samsarin.com/samsarin-php-widget
+Plugin URI:     http://samsarin.com/samsarin-php-widget
 Description:    A text widget with support for including PHP.
 Author:         Chris Pettitt
-Version:        1.3.2
+Version:        2.0b1
 Author URI:     http://samsarin.com
 
+2.0b1 Add support for WP 2.8, drop support for anything earlier.
 1.3.2 Add an options panel to reset all Samsarin PHP widgets.
 1.2   Increase max widgets to 25
 1.1   Workaround bug widget compatibility bug in WP 2.2
@@ -14,169 +15,69 @@ Author URI:     http://samsarin.com
 
 A text widget with support for including PHP.
 
-This plugin uses the MIT License. See the LICENSE file included
-with this distribution for more details.
+Copyright (C) 2007-2009 Chris Pettitt (http://samsarin.com). All rights reserved.
+
+This software is free to use. It may be redistributed provided the
+copyright is included. The author provides this software with no warranty and
+cannot be held liable for any results occurring from its use or misuse.
 */
 
-// Configuration
-// ----------------------------------------------------------------------------
-
-// Change this value to set the maximum number of widgets that can be displayed at one time.
-define('SPW_MAXWIDGETS', 25);
-
-// Don't edit below here
-// ----------------------------------------------------------------------------
-
-function spw_register_options() {
-    if (function_exists('add_options_page')) {
-        add_options_page("Samsarin PHP Widget Options", "Samsarin PHP", 10, __FILE__, 'spw_options_form'); 
-    }
-}
-
-function spw_options_form() {
-    $options = get_option('samsarin_php_widget');
-
-    if (isset($_POST['spw_reset_all_submit'])) {
-        for ( $i = 1; $i <= SPW_MAXWIDGETS; ++$i ) unset($options[$i]);
-        update_option('samsarin_php_widget', $options);
-    } elseif (isset($_POST['spw_reset_submit'])) {
-        $number = (int) $_POST['spw_number'];
-        if ($number > 0 && $number <= SPW_MAXWIDGETS) {
-            unset($options[$number]);
-            update_option('samsarin_php_widget', $options);
-        }
-    }
-?>
-    <div class="wrap">
-        <h2>Samsarin PHP Widget Options</h2>
-        <form method="post" onSubmit="return confirm('Are you sure you want to reset ALL Samsarin PHP Widgets?');">
-            <h3>Reset all Samsarin PHP Widgets</h3>
-            <p>Click 'Reset All' below to reset all Samsarin PHP Widgets to a default, empty state. <strong>You cannot undo this action</strong>,
-               so it is recommended that you bcakup your database before using it. If you want to reset specific widget, see the 
-               next section.</p>
-            <span class="submit"><input type="submit"  name="spw_reset_all_submit" value="<?php _e('Reset All'); ?>" /></span></p>
-        </form>
-        <form method="post" onSubmit="return confirm('Are you sure you want to reset this Samsarin PHP Widget?');">
-            <h3>Reset specific Samsarin PHP Widget</h3>
-            <p>Select the nubmer of a Samsarin PHP widget to reset to a default, empty state and click 'Reset' below. <strong>You cannot undo
-               this action</strong>, so it is recommended that you backup your database before using it. If you want to reset all widgets, 
-               please see the previous section.</p>
-            <p><?php _e('Reset Samsarin PHP widget:', 'widgets'); ?>
-            <select id="spw_number" name="spw_number" value="<?php echo $options['number']; ?>">
-                <option value='' selected='true'></option>
-<?php for ( $i = 1; $i <= $options['number']; ++$i ) echo "<option value='$i'>Samsarin PHP Widget $i: {$options[$i]['title']}</option>"; ?>
-            </select>
-            <span class="submit"><input type="submit" name="spw_reset_submit" value="<?php _e('Reset'); ?>" /></span></p>
-        </form>
-    </div>
-<?php
-}
-
-function spw_widget($args, $number = 1) {
-    extract($args);
-    $options = get_option('samsarin_php_widget');
-
-    $title = $options[$number]['title'];
-
-    $body = $options[$number]['body'];
-    if (empty($body)) {
-        $body = '&nbsp;';
+class SamsarinPhpWidget extends WP_Widget {
+    function SamsarinPhpWidget() {
+        $widget_ops = array('classname' => 'samsarin_php_widget', 'description' => 'A text widget with support for including PHP');
+        $control_ops = array('width' => 400, 'height' => 350);
+        $this->WP_Widget('samsarin_php_widget', 'Samsarin PHP Widget', $widget_ops, $control_ops);
     }
 
-    print $before_widget; 
-    
-    if (!empty($title)) {  
-        print $before_title;  
-        eval(" ?> $title <?php "); 
-        print $after_title;
-    }
+    function widget($args, $instance) {     
+        extract($args);
 
-    eval(" ?> $body <?php ");
-    print $after_widget;     
-}
+        $title = $instance['title'];
 
-function spw_control($number) {
-    $options = get_option('samsarin_php_widget');
-    if (!is_array($options)) {
-        $options = array();
-    }
-
-    if ($_POST["samsarin_widget_submit_$number"]) {
-        $options[$number]['title'] = stripslashes($_POST["samsarin_widget_title_$number"]);
-        $options[$number]['body'] = stripslashes($_POST["samsarin_widget_body_$number"]);
-    }
-
-    update_option('samsarin_php_widget', $options);
-
-    $title = htmlspecialchars($options[$number]['title'], ENT_QUOTES);
-    $body = htmlspecialchars($options[$number]['body'], ENT_QUOTES);
-?>          
-    <dl>
-        <dt><strong>Title</strong></dt>
-        <dd>
-            <input style="width: 350px; height: 50px;" name="samsarin_widget_title_<?php echo "$number"; ?>" type="text" value="<?php echo $title; ?>" />
-        </dd>
-        <dt><strong>Body<strong></dt>
-        <dd>
-            <textarea style="width: 350px; height: 200px;" name="samsarin_widget_body_<?php echo "$number"; ?>"><?php echo $body; ?></textarea>
-        </dd>
-    </dl> 
-    <input type="hidden" name="samsarin_widget_submit_<?php echo "$number"; ?>" value="1" />
-<?php
-}
-
-function spw_admin_setup() {
-    $options = get_option('samsarin_php_widget');
-    if (isset($_POST['samsarin_php_widget_number_submit'])) {
-        $number = (int) $_POST['samsarin_php_widget_number'];
-        if ( $number < 1 ) $number = 1;
-        if ($number > SPW_MAXWIDGETS) $number = SPW_MAXWIDGETS;
-        $options['number'] = $number;
-    }
-
-    update_option('samsarin_php_widget', $options);
-    spw_register_plugin($options['number']);
-}
-
-function spw_admin_page() {
-    $options = get_option('samsarin_php_widget');
-?>
-    <div class="wrap">
-        <h2><?php _e('Samsarin PHP Widgets', 'widgets'); ?></h2>
-        <form method="POST">
-            <p style="line-height: 30px;"><?php _e('How many Samsarin PHP widgets would you like?', 'widgets'); ?>
-            <select id="samsarin_php_widget_number" name="samsarin_php_widget_number" value="<?php echo $options['number']; ?>">
-<?php for ( $i = 1; $i <= SPW_MAXWIDGETS; ++$i ) echo "<option value='$i' ".($options['number']==$i ? "selected='selected'" : '').">$i</option>"; ?>
-            </select>
-            <span class="submit"><input type="submit" name="samsarin_php_widget_number_submit" value="<?php _e('Save'); ?>" /></span></p>
-        </form>
-    </div>
-<?php
-}
-
-function spw_register_plugin() {
-    global $wp_version;
-    if (function_exists('register_sidebar_widget') && function_exists('register_widget_control')) {
-        $options = get_option('samsarin_php_widget');
-        $number = $options['number'];
-        if ( $number < 1 ) $number = 1;
-        if ($number > SPW_MAXWIDGETS) $number = SPW_MAXWIDGETS;
-        for ($i = 1; $i <= SPW_MAXWIDGETS; $i++) {
-            $name = array('Samsarin PHP %s', 'samsarin', $i);
-            // Hack to support broken compatibility in WP 2.2
-            if ($wp_version == "2.2") {
-                register_sidebar_widget($name, $i <= $number ? 'spw_widget' : '', 'spw_widget', $i);
-            } else {
-                register_sidebar_widget($name, $i <= $number ? 'spw_widget' : '', $i);
-            }
-            register_widget_control($name, $i <= $number ? 'spw_control' : '', 460, 350, $i);
+        $body = $instance['body'];
+        if (empty($body)) {
+            $body = '&nbsp;';
         }
 
-        add_action('sidebar_admin_setup', 'spw_admin_setup');
-        add_action('sidebar_admin_page', 'spw_admin_page');
+        print $before_widget;
+
+        if (!empty($title)) {
+            print $before_title;
+            eval(" ?> $title <?php ");
+            print $after_title;
+        }
+
+        eval(" ?> $body <?php ");
+        print $after_widget;
     }
+
+    function update($new_instance, $old_instance) {             
+        return $new_instance;
+    }
+
+    function form($instance) {              
+        $title = $instance['title'];
+        $body = $instance['body'];
+
+        $title_id = $this->get_field_id('title');
+        $title_name = $this->get_field_name('title');
+
+        $body_id = $this->get_field_id('body');
+        $body_name = $this->get_field_name('body');
+
+?>
+        <p>
+            <label for="<?php echo $title_id; ?>">Title:</label>
+            <input class="widefat" id="<?php echo $title_id; ?>" name="<?php echo $title_name; ?>"
+                   type="text" value="<?php echo esc_attr($title); ?>"/>
+        </p>
+        <p>
+            <textarea class="widefat" id="<?php echo $body_id; ?>" name="<?php echo $body_name; ?>"
+                      rows="16" cols="20"><?php echo htmlspecialchars($body); ?></textarea>
+        </p>
+<?php 
+    }
+
 }
 
-add_action('plugins_loaded', 'spw_register_plugin'); 
-add_action('admin_menu', 'spw_register_options');
-?>
+add_action('widgets_init', create_function('', 'return register_widget("SamsarinPhpWidget");'));
